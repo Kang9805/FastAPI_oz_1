5be486a271fe9864d8c881f7e7b4c401ac534ce1
kang9805 <zzionkk@naver.com>
2025-08-19 19:26:28 +0900

    FastAPI 2일차 level 중

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 # main.py
 from typing import Annotated
 from fastapi import FastAPI, HTTPException, Path, Query
+from app.models.movies import MovieModel
 from app.models.users import UserModel
+from app.schemas.movies import MovieResponse, CreateMovieRequest, MovieSearchParams, MovieUpdateRequest
 from app.schemas.users import UserCreateRequest, UserUpdateRequest, UserSearchParams
 
 
 app = FastAPI()
 
 UserModel.create_dummy() # API 테스트를 위한 더미를 생성하는 메서드 입니다.
+UserModel.create_dummy()
+MovieModel.create_dummy()
 
 
-if __name__ == '__main__':
-    import uvicorn
 
 # 유저 생성 API
 @app.post('/users')
@@ -62,6 +64,50 @@ async def search_users(query_params: Annotated[UserSearchParams, Query()]):
     if not filtered_users:
         raise HTTPException(status_code=404)
     return 	filtered_users
+
+
+
+
+# 영화 등록 API
+@app.post('/movies', response_model=MovieResponse, status_code=201)
+async def create_movie(data: CreateMovieRequest):
+    movie = MovieModel.create(**data.model_dump())
+    return movie
+
+# 전체 영화 검색 및 리스트 조회 API
+@app.get('/movies', response_model=list[MovieResponse], status_code=200)
+async def get_movie(query_params: Annotated[MovieSearchParams, Query()]):
+    valid_query = {key: value for key, value in query_params.model_dump().items() if value is not None}
+
+    if valid_query:
+        return MovieModel.filter(**valid_query)
+    return MovieModel.all()
+
+# 특정 영화 상세 조회 API
+@app.get('/movies/{movie_id}', response_model=MovieResponse, status_code=200)
+async def get_movie(movie_id: int = Path(gt=0)):
+    movie = MovieModel.get(id=movie_id)
+    if movie is None:
+        raise HTTPException(status_code=404)
+    return movie
+
+# 특정 영화 정보 수정 API
+@app.patch('/movies/{movie_id}', response_model=MovieResponse, status_code=200)
+async def edit_movie(data: MovieUpdateRequest, movie_id: int = Path(gt=0)):
+	movie = MovieModel.get(id=movie_id)
+	if movie is None:
+		raise HTTPException(status_code=404)
+	movie.update(**data.model_dump())
+	return movie
+
+# 특정 영화 정보 삭제 API
+@app.delete('/movies/{movie_id}', status_code=204)
+async def delete_movie(movie_id: int = Path(gt=0)):
+	movie = MovieModel.get(id=movie_id)
+	if movie is None:
+		raise HTTPException(status_code=404)
+	movie.delete()
+	return
 
 
 if __name__ == '__main__':
